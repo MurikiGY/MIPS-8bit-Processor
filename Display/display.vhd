4aceb50fe3b94cf88329e1ee02b2bb77df2c75f1
Muriki <mgy20@inf.ufpr.br>
2022-08-28 23:56:23 -0300

    Fixed negative display

--- Display/display.vhd
+++ Display/display.vhd
@@ -20,22 +20,23 @@ begin
             when "0011" => cathodes <= "10110000"; -- 3
             when "0100" => cathodes <= "10011001"; -- 4
             when "0101" => cathodes <= "10010010"; -- 5
-            when "0110" => cathodes <= "10000011"; -- 6
+            when "0110" => cathodes <= "10000010"; -- 6
             when "0111" => cathodes <= "11111000"; -- 7
-
-            when "1000" => cathodes <= "10001001"; -- -8 H
-            when "1001" => cathodes <= "10010100"; -- -7 G
-            when "1010" => cathodes <= "10001110"; -- -6 F
-            when "1011" => cathodes <= "10000110"; -- -5 E
-            when "1100" => cathodes <= "10100001"; -- -4 d
-            when "1101" => cathodes <= "11000110"; -- -3 C
-            when "1110" => cathodes <= "10010011"; -- -2 b
-            when "1111" => cathodes <= "10001000"; -- -1 A
+				
+			when "1000" => cathodes <= "01111001"; -- -1
+            when "1001" => cathodes <= "00100100"; -- -2
+            when "1010" => cathodes <= "00110000"; -- -3
+            when "1011" => cathodes <= "00011001"; -- -4
+            when "1100" => cathodes <= "00010010"; -- -5
+            when "1101" => cathodes <= "00000010"; -- -6
+            when "1110" => cathodes <= "01111000"; -- -7
+            when "1111" => cathodes <= "00000000"; -- -8
 
             when others => cathodes <= "11111111"; --desliga
         end case;
     end process;
 
-    anodes <= "1110";
+    anodes <= "0000";
 
 end comportamento;
+
