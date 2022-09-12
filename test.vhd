3e04ab0e09f0e092d7ea98a2e407a424f4f4907c
Muriki <mgy20@inf.ufpr.br>
2022-09-12 03:08:15 -0300

    Added circuit diagram]

---
+++ test.vhd
@@ -0,0 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity something is
+    port(
+        signal1     : in    std_logic;
+        signal2     : out   std_logic
+    );
+end something;
+
+architecture rtl of something is
+
+    #Constants
+    #signals
+
+begin
+
+
+
+
+
+end something;
